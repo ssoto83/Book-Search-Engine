046442c018dc7f033df3bc34330655d81d82a4e0
ssoto83 <168875470+ssoto83@users.noreply.github.com>
2024-11-06 21:44:20 -0500

    updated the auth.js, vite.config and package.json

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,13 +5,13 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000,
+    port: 3004,
     open: true,
     proxy: {
-      '/api': {
-        target: 'http://localhost:3001',
+      '/graphql': {
+        target: 'http://localhost:3004',
         secure: false,
-        changeOrigin: true
+        changeOrigin: true,
       }
     }
   }
