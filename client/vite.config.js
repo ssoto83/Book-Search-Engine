b19de225d8152a8526b8309b0485164d71886f61
ssoto83 <168875470+ssoto83@users.noreply.github.com>
2024-11-07 00:03:57 -0500

    changed the port in vite.config

--- client/vite.config.js
+++ client/vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
     open: true,
     proxy: {
       '/graphql': {
-        target: 'http://localhost:3004',
+        target: 'http://localhost:3005',
         secure: false,
         changeOrigin: true,
       }
